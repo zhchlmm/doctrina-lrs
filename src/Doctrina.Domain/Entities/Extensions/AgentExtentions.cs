d3455545b4e9ceafee8f54b00c2e5d1d84ef0817
rasmuseeg <rasmuseeg@outlook.com>
2019-05-28 20:52:35 +0200

    Added IEnumerable version of the WhereAgent extension

--- src/Doctrina.Domain/Entities/Extensions/AgentExtentions.cs
+++ src/Doctrina.Domain/Entities/Extensions/AgentExtentions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Doctrina.Domain.Entities.Extensions
@@ -7,6 +8,35 @@ namespace Doctrina.Domain.Entities.Extensions
     {
         public static IQueryable<T> WhereAgent<T>(this IQueryable<T> profiles, Func<T, AgentEntity> agentSelector, AgentEntity agent)
         {
+            profiles = profiles.Where(p => agentSelector(p).ObjectType == agent.ObjectType);
+
+            if (string.IsNullOrEmpty(agent.Mbox))
+            {
+                return profiles.Where(p => agentSelector(p).Mbox == agent.Mbox);
+            }
+
+            if (string.IsNullOrEmpty(agent.Mbox_SHA1SUM))
+            {
+                return profiles.Where(p => agentSelector(p).Mbox_SHA1SUM == agent.Mbox_SHA1SUM);
+            }
+
+            if (string.IsNullOrEmpty(agent.OpenId))
+            {
+                return profiles.Where(p => agentSelector(p).OpenId == agent.OpenId);
+            }
+
+            if (string.IsNullOrEmpty(agent.Account.HomePage))
+            {
+                return profiles.Where(p => agentSelector(p).Account.HomePage == agent.Account.HomePage && agentSelector(p).Account.Name == agent.Account.Name);
+            }
+
+            return profiles;
+        }
+
+        public static IEnumerable<T> WhereAgent<T>(this IEnumerable<T> profiles, Func<T, AgentEntity> agentSelector, AgentEntity agent)
+        {
+            profiles = profiles.Where(p => agentSelector(p).ObjectType == agent.ObjectType);
+
             if (string.IsNullOrEmpty(agent.Mbox))
             {
                 return profiles.Where(p => agentSelector(p).Mbox == agent.Mbox);
