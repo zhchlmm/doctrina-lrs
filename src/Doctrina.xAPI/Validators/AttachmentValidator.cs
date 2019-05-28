936ed66836522cc07d380e2de90740ed50c4572f
rasmuseeg <rasmuseeg@outlook.com>
2019-05-28 20:50:26 +0200

    Updated attachment validator

--- src/Doctrina.xAPI/Validators/AttachmentValidator.cs
+++ src/Doctrina.xAPI/Validators/AttachmentValidator.cs
@@ -4,6 +4,28 @@ namespace Doctrina.xAPI.Validators
 {
     public class AttachmentValidator : AbstractValidator<Attachment>
     {
+        public AttachmentValidator()
+        {
+            RuleFor(x => x.UsageType).NotEmpty();
+            RuleFor(x => x.Display).NotEmpty();
+            RuleFor(x => x.ContentType).NotEmpty();
+            RuleFor(x => x.Length).NotEmpty();
+            RuleFor(x => x.SHA2).NotEmpty();
 
+            // TODO: Create validation rules for the validation of Signatures
+            //if (UsageType == new Iri("http://adlnet.gov/expapi/attachments/signature")
+            //    && ContentType == "application/octet-stream")
+            //{
+            //    // Verify signatures are well formed
+
+            //    // Decode the JWS signature, and load the signed serialization of the Statement from the JWS signature payload.
+
+            //    // Validate that the original Statement is logically equivalent to the received Statement.
+
+            //    // If the JWS header includes an X.509 certificate, validate the signature against that certificate as defined in JWS.
+
+            //    // Validate that the signature requirements outlined above have been met.
+            //}
+        }
     }
 }
