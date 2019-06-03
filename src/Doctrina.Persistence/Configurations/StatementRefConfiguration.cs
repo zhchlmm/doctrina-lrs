9a0c56ecfbf8a4612b4f90aa0b475ad57916e250
rasmuseeg <rasmuseeg@outlook.com>
2019-06-03 20:17:29 +0200

    Added StatementRef entity configuration

---
+++ src/Doctrina.Persistence/Configurations/StatementRefConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Doctrina.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Doctrina.Persistence.Configurations
+{
+    public class StatementRefConfiguration : IEntityTypeConfiguration<StatementRefEntity>
+    {
+        public void Configure(EntityTypeBuilder<StatementRefEntity> builder)
+        {
+            builder.Property(x => x.StatementRefId)
+                .ValueGeneratedOnAdd();
+            builder.HasKey(x => x.StatementRefId);
+
+            builder.Property(x => x.Id)
+                .IsRequired();
+            builder.HasIndex(x => x.Id)
+                .ForSqlServerIsClustered();
+        }
+    }
+}
