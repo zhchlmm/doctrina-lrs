38a3bf3a1f8ef8b0f3b14bbbe5a39f84d0471162
rasmuseeg <rasmuseeg@outlook.com>
2019-06-07 09:58:08 +0200

    Re-added EntityTagHeader

---
+++ src/Doctrina.xAPI.LRS/Mvc/Models/EntityTagHeader.cs
@@ -0,0 +1,10 @@
+﻿using Doctrina.xAPI.Client.Http;
+
+namespace Doctrina.xAPI.Store.Mvc.Models
+{
+    public class EntityTagHeader
+    {
+        public string Tag { get; internal set; }
+        public ETagMatch Match { get; internal set; }
+    }
+}
